a009ad8965c2d1b8580380bd4cdca31957b8160c
Darnell Otterson <rfolkker@gmail.com>
2024-01-03 22:24:21 -0600

    Finished basic UI structure
    Switched from considering React to just using Bootstrap, no need to over-complicate things for the sake of a common standard.

--- app.py
+++ app.py
@@ -1,8 +1,13 @@
-from waitress import serve
-import falcon
-from Model import RootResource
-import Model
+#!/usr/bin/python3
 
+# WSGI
+from waitress import serve
+# Renderer
+from jinja2 import Environment
+# Web Framework
+import falcon
+
+#Custom classes
 import renderer
 import config
 
@@ -11,7 +16,10 @@ app_config = config.ConfigHandler("config.ini")
 app = falcon.App()
 template = renderer.ui(app_config.config)
 script_handler = renderer.script(app_config.config)
+favicon_handler = renderer.FaviconResource(app_config.config)
 app.add_route("/{page}", template)
 app.add_route("/static/{script}",script_handler)
+app.add_route('/favicon.ico', favicon_handler)
+
 if __name__ == '__main__':
    serve(app, host=app_config.config.get("bind_address",'0.0.0.0'), port=int(app_config.config.get("port",8080)))
--- renderer.py
+++ renderer.py
@@ -1,28 +1,47 @@
-from jinja2 import Template
+from jinja2 import Environment, FileSystemLoader
 import falcon
 import os
 from os import listdir
 from os.path import isfile, join
+
 class script:
    config = None
+   script_root = "scripts/"
 
    def __init__(self, config):
       self.config = config
+      self.script_root = self.config.get("scripts", self.script_root)
 
    def on_get(self, req, resp, script):
-    """Handles GET requests"""
     resp.content_type = falcon.MEDIA_JS
-    with open(f"Scripts/{script}","r") as fp:
+    script_file = join(self.script_root,script)
+    with open(script_file,"r") as fp:
       resp.body = fp.read()
+
+class FaviconResource:
+   config = None
+   favicon_file = "favicon.ico"
+   def __init__(self, config):
+      self.config = config
+      self.favicon_file = join(self.config.get("images", "images/"), self.config.get("favicon", self.favicon_file))
+
+   def on_get(self, req, resp):
+      # Replace 'path/to/favicon.ico' with the actual path to your favicon file
+      with open(self.favicon_file, 'rb') as f:
+         resp.data = f.read()
+      resp.content_type = falcon.MEDIA_PNG  # Set the correct content type for your favicon
 
 class ui:
    config = None
    routes = {}
-   path_root = "Template/"
-
+   path_root = "templates/"
+   template_env = None
    def __init__(self, config):
       self.config = config
       self.path_root = self.config.get("templates", self.path_root)
+
+      self.template_env = Environment(loader=FileSystemLoader(self.path_root))
+
       for f in listdir(self.path_root):
          if isfile(join(self.path_root, f)) and f.endswith(".html"):
             print(f"{f} found")
@@ -36,9 +55,8 @@ class ui:
    def get_page(self, name, data):
       result = None
       if name in self.routes:
-        with open(self.routes[name],"r") as fp:
-            tempobj=Template(fp.read())
-            result = tempobj.render(data)
+        template = self.template_env.get_template(f"{name}.html")
+        result = template.render(data)
       return result
    
    def validate_page(self, page):
@@ -70,4 +88,4 @@ class ui:
               resp.status = falcon.HTTP_500
         else:
               resp.status = falcon.HTTP_200
-              resp.body = result
+              resp.text = result
